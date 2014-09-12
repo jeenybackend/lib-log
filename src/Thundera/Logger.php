f9fd4b0714929d7f20e1e0230d9a072f9f940b8f
Leandro Augusto <leandro.freire@easytaxi.com.br>
2014-09-12 14:57:16 -0300

    Testing send message

--- src/Thundera/Logger.php
+++ src/Thundera/Logger.php
@@ -1,22 +1,35 @@
 <?php
 
+namespace Thundera;
+
 use Gelf\Logger as GelfLogger;
 use Gelf\Transport\UdpTransport;
 use Gelf\Publisher;
+use Gelf\Message;
 
 class Logger extends GelfLogger{
 
     protected $_publisher;
 
-
     public function __construct()
     {
         $transport = new UdpTransport("127.0.0.1", 12202, UdpTransport::CHUNK_SIZE_LAN);
 
-        $publisher = new Publisher();
-        $publisher->addTransport($transport);
+        $this->_publisher = new Publisher();
+        $this->_publisher->addTransport($transport);
 
-        parent::__construct($publisher);
+        parent::__construct($this->_publisher);
+    }
+
+
+    public static function send()
+    {
+        $message = new Message();
+        $message->setShortMessage("Foobar!")
+            ->setLevel(\Psr\Log\LogLevel::ALERT)
+            ->setFullMessage("There was a foo in bar")
+            ->setFacility("example-facility");
+        self::$_publisher->publish($message);
     }
 
 }
